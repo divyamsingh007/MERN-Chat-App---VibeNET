562c12592ade0ffa47982bc15fcf1cf518ceebc6
divyamsingh007 <legendprice007@gmail.com>
2025-09-05 15:25:17 +0530

    Added assets, and initialised sidebar

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
 
 function App() {
 
   return (
-    <>
-      <h1>VibeNet</h1>
-    </>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
