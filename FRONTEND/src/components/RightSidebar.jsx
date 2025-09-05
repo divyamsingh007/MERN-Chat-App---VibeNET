df22301fee4fa59d0e5e4067e6a98959790c9814
divyamsingh007 <legendprice007@gmail.com>
2025-09-06 02:07:56 +0530

    Initialized the login, signup page and minor fixes

--- FRONTEND/src/components/RightSidebar.jsx
+++ FRONTEND/src/components/RightSidebar.jsx
@@ -4,7 +4,11 @@ import assets, { imagesDummyData } from "../assets/assets";
 export default function RightSidebar({ selectedUser }) {
   return (
     selectedUser && (
-      <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""}`}>
+      <div
+        className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${
+          selectedUser ? "max-md:hidden" : ""
+        }`}
+      >
         <div className="pt-16 flex flex-col  items-center gap-2 text-xs font-light mx-auto">
           <img
             src={selectedUser?.profilePic || assets.avatar_icon}
@@ -17,18 +21,24 @@ export default function RightSidebar({ selectedUser }) {
           </h1>
           <p className="px-10 mx-auto">{selectedUser.bio}</p>
         </div>
-        <hr className="border-[#ffffff50] my-4"/>
+        <hr className="border-[#ffffff50] my-4" />
         <div className="px-5 text-xs">
           <p>Media</p>
           <div className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80">
-            {imagesDummyData.map((url, index)=>(
-              <div key={index} onClick={()=> window.open(url)} className="cursor-pointer rounded">
+            {imagesDummyData.map((url, index) => (
+              <div
+                key={index}
+                onClick={() => window.open(url)}
+                className="cursor-pointer rounded"
+              >
                 <img src={url} alt="" className="h-full rounded-md" />
               </div>
             ))}
           </div>
         </div>
-        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">Logout</button>
+        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+          Logout
+        </button>
       </div>
     )
   );
