26bd7a132f1014f6df4882f38e0e466331f53721
divyamsingh007 <legendprice007@gmail.com>
2025-09-11 20:40:56 +0530

    connecting backend to frontend

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import ProfilePage from './pages/ProfilePage.jsx'
+import { useContext, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import { AuthContext } from "../context/AuthContext.jsx";
 
 function App() {
+  const { authUser } = useContext(AuthContext);
 
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster></Toaster>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={
+            authUser ? <HomePage /> : <Navigate to={"/login"}></Navigate>
+          }
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"}></Navigate>}
+        />
+        <Route
+          path="/profile"
+          element={
+            authUser ? <ProfilePage /> : <Navigate to={"/login"}></Navigate>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,11 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { AuthProvider } from "../context/AuthContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
