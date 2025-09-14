aa1d772440713a69ec62643f7ff484d6b472b2b4
divyamsingh007 <legendprice007@gmail.com>
2025-09-14 13:18:34 +0530

    working on deploying it to the vercel

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -21,20 +21,20 @@ export const io = new Server(server, {
 export const userSocketMap = {}; // {userId: socketId}
 
 //Socket.io connection handler
-io.on("connection", (socket)=> {
-    const userId= socket.handshake.query.userId
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
 
-    if(userId) userSocketMap[userId] = socket.id
+  if (userId) userSocketMap[userId] = socket.id;
 
-    //Emit online users to all connected clients
-    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  //Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
-        console.log(`User disconnected: ${userId}`)
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys[userSocketMap])
-    })
-})
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${userId}`);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys[userSocketMap]);
+  });
+});
 
 // Middleware Setup
 APP.use(express.json({ limit: "4mb" }));
@@ -48,6 +48,11 @@ APP.use("/api/messages", messageRouter);
 //Connect the databse
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server is running on port: ${PORT}~`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log(`Server is running on port: ${PORT}~`));
+}
+
+//Export server for vercel
+export default server;
